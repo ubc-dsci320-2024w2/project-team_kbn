e138ae731c32038d15fa1bf99fde6b88675bb783
Nick-2003 <tsunlinicholastam@gmail.com>
2025-03-05 20:02:38 -0800

    More reorganising

--- code/data_clean.py
+++ code/data_clean.py
@@ -16,29 +16,29 @@ public_trees_cleaned[['TREE_ID', 'CIVIC_NUMBER', 'ON_STREET_BLOCK']] = public_tr
 
 print("Set to correct data types")
 
+# Earlier EDA provided `CULTIVAR_NAME`, `NEIGHBOURHOOD_NAME`, `DATE_PLANTED` as having NA values; `CULTIVAR_NAME` is filled with the corresponding `SPECIES_NAME`, `NEIGHBOURHOOD_NAME` has `NA` strings applied instead, and `DATE_PLANTED` is left with `None`
+public_trees_cleaned['CULTIVAR_NAME'] = public_trees_cleaned['CULTIVAR_NAME'].fillna(public_trees_cleaned['SPECIES_NAME'])
+public_trees_cleaned['NEIGHBOURHOOD_NAME'] = public_trees_cleaned['NEIGHBOURHOOD_NAME'].fillna('NA')
+public_trees_cleaned['DATE_PLANTED'] = public_trees_cleaned['DATE_PLANTED'].fillna('')
+print("NAs filled")
+
 # Latitude and longitude are initially joined together in a single column geo_point_2d and thus need to be split
 public_trees_cleaned[['LATITUDE', 'LONGITUDE']] = public_trees_cleaned['geo_point_2d'].str.split(', ', expand=True)
 public_trees_cleaned['LATITUDE'] = public_trees_cleaned['LATITUDE'].astype(float)
 public_trees_cleaned['LONGITUDE'] = public_trees_cleaned['LONGITUDE'].astype(float)
 print("Cleaned LATITUDE and LONGITUDE")
 
+# The values 9, 0 and 10 all correspond to HEIGHT_RANGE = '>90' and thus 0 and 10 are replaced with value 9
+public_trees_cleaned['HEIGHT_RANGE_ID'] = public_trees_cleaned['HEIGHT_RANGE_ID'].replace([0, 10], 9)
+print("HEIGHT_RANGE_ID set")
+
 # `NOMENCLATURE` is a column derived from combining `GENUS_NAME` and `SPECIES_NAME` into 1 string
 public_trees_cleaned['NOMENCLATURE'] = public_trees_cleaned['GENUS_NAME'] + " " + public_trees_cleaned['SPECIES_NAME']
 print("Created NOMENCLATURE")
 
 # `ON_ADDRESS` is a column derived from combining `ON_STREET` and `ON_STREET_BLOCK` into 1 string
-public_trees_cleaned['ON_ADDRESS'] = public_trees_cleaned['ON_STREET'] + " " + public_trees_cleaned['ON_STREET_BLOCK']
+public_trees_cleaned['ON_ADDRESS'] = public_trees_cleaned['ON_STREET_BLOCK'] + " " + public_trees_cleaned['ON_STREET'] + " " + public_trees_cleaned['NEIGHBOURHOOD_NAME'] + " (" + public_trees_cleaned['STREET_SIDE_NAME'] + ")"
 print("Created ON_ADDRESS")
-
-# The values 9, 0 and 10 all correspond to HEIGHT_RANGE = '>90' and thus 0 and 10 are replaced with value 9
-public_trees_cleaned['HEIGHT_RANGE_ID'] = public_trees_cleaned['HEIGHT_RANGE_ID'].replace([0, 10], 9)
-print("HEIGHT_RANGE_ID set")
-
-# Earlier EDA provided 'CULTIVAR_NAME', 'NEIGHBOURHOOD_NAME', 'DATE_PLANTED' as having NA values; 'CULTIVAR_NAME' is filled with the corresponding 'SPECIES_NAME', while 'NEIGHBOURHOOD_NAME', 'DATE_PLANTED' have 'NA' strings applied instead
-public_trees_cleaned['CULTIVAR_NAME'] = public_trees_cleaned['CULTIVAR_NAME'].fillna(public_trees_cleaned['SPECIES_NAME'])
-public_trees_cleaned['NEIGHBOURHOOD_NAME'] = public_trees_cleaned['NEIGHBOURHOOD_NAME'].fillna('')
-public_trees_cleaned['DATE_PLANTED'] = public_trees_cleaned['DATE_PLANTED'].fillna('')
-print("NAs filled")
 
 # Due to being redundant and difficult to use, 'Geom' and 'geo_point_2d' are dropped
 public_trees_cleaned = public_trees_cleaned.drop(columns=['Geom', 'geo_point_2d'])
